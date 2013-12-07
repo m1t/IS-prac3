53c0771036b82b2f60bbda1ab1bdd9c55c44f75a
mits <mits.jp@gmail.com>
2013-12-07 23:11:27 +0900

    演習３作成

--- IS-prac3/ViewController.h
+++ IS-prac3/ViewController.h
@@ -10,4 +10,6 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)clickAction:(id)sender;
+
 @end
--- IS-prac3/ViewController.m
+++ IS-prac3/ViewController.m
@@ -26,4 +26,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)clickAction:(id)sender
+{
+    // Hello Worldのメッセージをアラート表示する
+    UIAlertView *alert =
+    [[UIAlertView alloc] initWithTitle:@"Hello World" // アラートのタイトル
+                               message:@"Hello iOS app!" // アラートのメッセージ
+                              delegate:self // 結果を受け取るインスタンス
+                     cancelButtonTitle:@"OK" // ボタンに記載される文字列
+                     otherButtonTitles:nil];
+    [alert show];
+}
+
 @end
